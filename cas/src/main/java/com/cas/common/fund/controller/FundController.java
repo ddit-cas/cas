cada7aeb074ecc295ed863a48311b0aaf171c2e4
park <kim@naver.com>
2017-07-11 21:16:40 +0900

    c:controller 2017.07.11-bbind

--- cas/src/main/java/com/cas/common/fund/controller/FundController.java
+++ cas/src/main/java/com/cas/common/fund/controller/FundController.java
@@ -1,6 +1,9 @@
 package com.cas.common.fund.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,13 +11,19 @@ public class FundController {
 
 	/*현재 진행중인 크라우드 펀딩목록으로 가는 메서드*/
 	@RequestMapping("/fundList")
-	public String fundList(){
+	public String fundList(Model model){
 		return null;
 	}
 	
 	/*종료된 크라우드펀딩목록으로 가는 메서드*/
 	@RequestMapping("/endFundList")
-	public String endFundList(){
+	public String endFundList(Model model,HttpServletRequest request){
+		return null;
+	}
+	
+	/*크라우드펀딩 상세 내용으로 가는 메서드*/
+	@RequestMapping("/fundDetail")
+	public String fundDetail(Model model,HttpServletRequest request){
 		return null;
 	}
 }
---
+++ cas/src/main/java/com/cas/member/ucc/controller/MemberUccController.java
@@ -0,0 +1,39 @@
+package com.cas.member.ucc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberUccController {
+
+	/*
+	 * 유씻씨영상을 입력하거나 수정하는 양식으로 이동하는 메서드
+	 * 만약 수정을눌렀을시 글 아이디를 받아서 수정화면을 보여주고
+	 * 글아이디가 없으면 그냥 입력화면을보여줌
+	 */
+	@RequestMapping("/member/uccForm")
+	public String uccForm(HttpServletRequest request,Model model){
+		return null;
+	}
+	
+	/*유씨씨영상 입력양식을 다 입력후 등록을 누르면 인서트 해주는 메서드*/
+	@RequestMapping("/member/insertUcc")
+	public String insertFreeboard(HttpServletRequest request){
+		return null;
+	}
+	
+	/*글 수정양식을 다 입력한후 수정을 눌렀을떄 오는 메서드*/
+	@RequestMapping("/member/updateUcc")
+	public String updateFreeboard(HttpServletRequest request){
+		return null;
+	}
+	
+	/*회원 본인이 글 삭제를 눌렀을시 오는 메서드*/
+	@RequestMapping("/member/deleteUcc")
+	public String deleteFreeboard(HttpServletRequest request){
+		return null;
+	}
+}
